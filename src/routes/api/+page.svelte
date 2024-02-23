a1804c049256970076da271836722fe65bfc63b0
zmarabeas <zmarabeas@github.com>
2024-02-22 19:01:08 -0500

    server development

---
+++ src/routes/api/+page.svelte
@@ -0,0 +1,58 @@
+
+<script>
+	let name = null;
+	let birthday = null;
+
+	function handleSubmit(){
+		setItem(name, birthday);	
+		name = null;
+		birthday = null;
+	}
+</script>
+
+<div class=main>
+	<div class=col-container id=api>
+		<h1>API TESTING PAGE</h1>
+		<input class=inp bind:value={name} type=text 
+			placeholder="Enter Full Name">
+		<input class=inp bind:value={birthday} type=text 
+			placeholder="Enter Birthday">
+		<button class=btn on:click={()=>handleSubmit()}>submit</button>
+	</div>
+</div>
+
+<style>
+	.main {
+		display: flex;
+		//justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		min-width: 100vw;
+		min-height: 100vh;
+	}
+
+	.col-container {
+		display: flex;
+		//justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 10px;
+	}
+
+	.inp {
+		text-align: center;
+		border-radius: 5px;
+		width: 200px;
+		height: 25px;
+	}
+
+	.btn {
+		text-align: center;
+		border-radius: 5px;
+		width: 100px;
+		height: 25px;
+		background-color: purple;
+		color: azure;
+		border-style: none;
+	}
+</style>
--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,7 +1,22 @@
 
-export function GET({ url } : any) 
+//const admin = require("firebase-admin");
+
+//const serviceAccount = require("./serviceAccountKey.json");
+/***********************************************
+      ****************TODO****************      
+	1. Convert fb.cjs to web sdk functions
+	2. Test all server and database functions
+	3. Build simple front end
+***********************************************/
+
+export function GET({ request } : any) 
 {
-    let firstName : string = url.searchParams.get('firstName') ?? 'Default firstname';
-    let lastName : string = url.searchParams.get('lastName') ?? 'Default lastname';
-    return new Response("Hello " + firstName + " " + lastName);
+    let firstName : string = request.searchParams.get('firstName') 
+                                                ?? 'Default firstname';
+    let lastName : string  = request.searchParams.get('lastName')
+                                                ?? 'Default lastname';
+    return new Response(`Hello ${firstName} ${lastName}`);
 }
+
+
+
