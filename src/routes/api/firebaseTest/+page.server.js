5cfe1fd9783037fec9604739356406bc2354d924
zmarabeas <zmarabeas@gmail.com>
2024-02-26 16:25:19 -0500

    vercel testing

--- src/routes/api/firebaseTest/+page.server.js
+++ src/routes/api/firebaseTest/+page.server.js
@@ -2,12 +2,12 @@ import { writeUserData } from './index.js';
 
 export function GET({ request } : any) 
 {
-    let firstName : string = request.searchParams.get('firstName') ?? 'Default';
-    let lastName : string  = request.searchParams.get('lastName') ?? 'Default';
-    let userId : string = request.searchParams.get('userId') ?? 'Default';
+    let firstName= request.searchParams.get('firstName') ?? 'Default';
+    let lastName= request.searchParams.get('lastName') ?? 'Default';
+    let userId= request.searchParams.get('userId') ?? 'Default';
     console.log('GET request received');
     writeUserData(firstName, lastName, userId);
-    let ret: any = {
+    let ret = {
       status: 200,
       body: { 'firstName': firstName, 'lastName': lastName, 'userId': userId }
     };
