71cde008ae82432478b40f7f352d5707abb918bc
zmarabeas <zmarabeas@github.com>
2024-03-14 14:15:20 -0400

    update gitignore

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,25 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [2],
+		"/application": [3],
+		"/sell-my-car": [4]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
