e278a7fa60cbbebae5635d3ce44090c9f714dbde
zmarabeas <zmarabeas@github.com>
2024-03-14 14:22:32 -0400

    testing

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -2,8 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 // import { getAnalytics } from "firebase/analytics"; 
 import { firebaseConfig } from "./settings.js"; 
+import { getApps, getApp } from "firebase/app";
 
-const app = initializeApp(firebaseConfig);
+
+// if (!firebase.apps.length) {
+//     firebase.initializeApp({});
+//  }else {
+//     firebase.app(); // if already initialized, use that one
+//  }
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const app = initializeApp(firebaseConfig);
 
 // const FirebaseAnalytics = getAnalytics(app);
 const FirebaseDB = getDatabase(app);
--- src/lib/firebase/settings.js
+++ src/lib/firebase/settings.js
@@ -7,26 +7,26 @@ const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID;
 const appId = import.meta.env.VITE_APP_ID;
 const measurementId = import.meta.env.VITE_MEASUREMENT_ID;
 
-const firebaseConfig = {
-  apiKey,
-  authDomain,
-  databaseURL,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId,
-  measurementId
-};
-
 // const firebaseConfig = {
-//   apiKey: "AIzaSyC7ij7x2up4mqxJvnArSI-adaDxPhUzt0k",
-//   authDomain: "gns-fub-chat-45501.firebaseapp.com",
-//   databaseURL: "https://gns-fub-chat-45501-default-rtdb.firebaseio.com",
-//   projectId: "gns-fub-chat-45501",
-//   storageBucket: "gns-fub-chat-45501.appspot.com",
-//   messagingSenderId: "502025304899",
-//   appId: "1:502025304899:web:2fb48774eb1cf125744df6",
-//   measurementId: "G-19HXS5CKH2"
+//   apiKey,
+//   authDomain,
+//   databaseURL,
+//   projectId,
+//   storageBucket,
+//   messagingSenderId,
+//   appId,
+//   measurementId
 // };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7ij7x2up4mqxJvnArSI-adaDxPhUzt0k",
+  authDomain: "gns-fub-chat-45501.firebaseapp.com",
+  databaseURL: "https://gns-fub-chat-45501-default-rtdb.firebaseio.com",
+  projectId: "gns-fub-chat-45501",
+  storageBucket: "gns-fub-chat-45501.appspot.com",
+  messagingSenderId: "502025304899",
+  appId: "1:502025304899:web:2fb48774eb1cf125744df6",
+  measurementId: "G-19HXS5CKH2"
+};
 
 export { firebaseConfig }
