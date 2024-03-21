27f327ee16e74138b0d5d713d02bb383571b6bfd
zmarabeas <zmarabeas@github.com>
2024-03-20 21:02:08 -0400

    welcome banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,12 +23,25 @@
 
 <body>
   <div class=content>
-    <h2>No Dealerships. No Hassle.</h2>
-    <p>Get pre-approved to buy a car or sell your car directly to us.</p>
-    <div class=input-container>
-      <button class=btn on:click={()=>handleClick(applicationRoute)}>Get Pre-Approved</button>
-      <span>Or</span>
-      <button class=btn on:click={()=>handleClick(sellMyCarRoute)}>Sell My Car</button>
+      <h2>No Dealerships. No Hassle.</h2>
+      <p>Get pre-approved to buy a car or sell your car directly to us.</p>
+      <div class=input-container>
+        <button class=btn on:click={()=>handleClick(applicationRoute)}>Get Pre-Approved</button>
+        <span>Or</span>
+        <button class=btn on:click={()=>handleClick(sellMyCarRoute)}>Sell My Car</button>
+      </div>
+  </div>
+</body>
+
+<body>
+  <div class=content id=about>
+    <div class=wrapper id=company>
+      <div class=blur>
+        <h2>Welcome to Great North Auto Sales and Finance!</h2>
+        <div class=desc>
+          <p class=info>Sharing our expertise and planning for the future within the automotive sales, financing, powersport, recreation, commercial and rental industries.</p>
+        </div>
+      </div>
     </div>
   </div>
 </body>
@@ -53,6 +66,12 @@
     min-height: 80vh;
   }
 
+  .info {
+    /* padding: 5px;
+    border-radius: 15px; */
+    max-width: 50%;
+  }
+
   .content {
     display: flex;
     flex-direction: column;
@@ -60,4 +79,46 @@
     height: 100%;
     padding: 20px;
   }
+
+  .desc {
+  }
+
+  #about {
+    /* justify-content: center; */
+    align-items: center;
+    /* background-image: url('finance-background-image.webp'); */
+    /* max-width: 80%; */
+  }
+
+  .wrapper {
+    border: 1px solid azure;
+    border-radius: 15px;
+    /* padding: 20px; */
+    height: 100%;
+    max-width: 95%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    /* background-image: url('jeep-background-image.jpeg'); */
+    /* color: #80AE9F; */
+    background-image: url('driving-winter-image.jpeg');
+    background-size: cover;
+    background-position: center;
+    background-position-y: 60%;
+  }
+
+  .blur {
+    min-width: 100%;
+    min-height: 100%;
+    padding: 20px;
+    border-radius: 15px;
+    backdrop-filter: blur(1px);
+    -webkit-backdrop-filter: blur(1px);
+  }
+
+  #company {
+  }
 </style>
+
