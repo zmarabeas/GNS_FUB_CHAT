96fa136272b639a73386544d52bd28af6496217c
zmarabeas <zmarabeas@github.com>
2024-03-23 11:39:52 -0400

    input component

---
+++ src/lib/components/BIBinput.svelte
@@ -0,0 +1,115 @@
+
+<script>
+    export let width = 100;
+    export let value = null;
+    export let placeholder;
+    export let label='label';
+    export let type='text';
+    export let max;
+
+
+    function formatNumber(input, max) {
+        // Remove all non-numeric characters
+        let formattedString = input.replace(/\D/g, '');
+
+
+        // Cap at the maximum number of digits
+        if (max !== undefined && formattedString.length > max) {
+            formattedString = formattedString.slice(0, max);
+        }
+
+        // Insert comma every three digits from the right
+        formattedString = formattedString.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+        return formattedString;
+    }
+
+    function formatPhone(input) {
+        // Remove all non-numeric characters
+        let formattedString = input.replace(/\D/g, '');
+
+
+        // Cap at the maximum number of digits
+        if (max !== undefined && formattedString.length > 10) {
+            formattedString = formattedString.slice(0, 10);
+        }
+
+        // Insert a dash after the third digit and after the sixth digit
+        if (formattedString.length > 3) {
+            formattedString = formattedString.slice(0, 3) + '-' + formattedString.slice(3);
+        }
+
+        if (formattedString.length > 7) {
+            formattedString = formattedString.slice(0, 7) + '-' + formattedString.slice(7);
+        }
+
+
+        return formattedString;
+    }
+
+
+    let formattedValue = '';
+    $: {
+        // format value to commas every 3 digits
+        if(type==='number'){
+            formattedValue = formatNumber(value, max);
+            value = formattedValue;
+        }else if(type==='phone'){
+            formattedValue = formatPhone(value);
+            value = formattedValue;
+        }
+    }
+
+</script>
+
+<div class=bib-container style="max-width:{width}%">
+    <span class=bib-sp>{label} ❯ </span>
+    <input class=bib-inp type="text" placeholder={placeholder} bind:value>
+</div>
+
+
+<style>
+    .bib-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        background-color: azure;
+        color:black;
+        border-radius: 15px;
+        height: 100%;
+        /* padding: 20px; */
+        width: 100%;
+        padding: 20px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+    .bib-container:focus-within {
+        outline: 2px solid #183E31;
+    }
+
+    .bib-inp {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 10px;
+        color: black;
+        font-size: .8em;
+        border: none;
+        background-color: rgba(0, 0, 0, 0.0);
+        text-align: right;
+    }
+
+    .bib-inp:focus {
+        outline: none;
+    }
+
+    .bib-sp {
+        font-size: 1.2em;
+        text-wrap: nowrap;
+        align-items: center;
+        color: black;
+    }
+
+</style>
